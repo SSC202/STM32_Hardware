42d124256379ac6710024922a79e46dd2b8e52c1
SSC202 <e22750706642022@163.com>
2023-11-20 06:44:53 +0800

    环形FIFO

--- BSP_Bag/RingBuffer/ringbuffer.c
+++ BSP_Bag/RingBuffer/ringbuffer.c
@@ -12,12 +12,17 @@ static Ring_Buffer_State Ring_Buffer_State_Get(Ring_Buffer_t pbuf)
 {
     if (pbuf->read_pos == pbuf->write_pos) // 读写指针为相同位置，此时环形缓冲区仅能为空和满两种状态，此时将使用镜像位
     {
-        if (pbuf->read_mirror == pbuf->write_mirror) {
+        if (pbuf->read_mirror == pbuf->write_mirror)
+        {
             return RINGBUFFER_EMPTY; // 如果读写指针镜像位相同，则缓冲区为空，反之则满
-        } else {
+        }
+        else
+        {
             return RINGBUFFER_FULL;
         }
-    } else {
+    }
+    else
+    {
         return RINGBUFFER_HALFFULL;
     }
 }
@@ -29,19 +34,23 @@ static Ring_Buffer_State Ring_Buffer_State_Get(Ring_Buffer_t pbuf)
  */
 static int16_t Ring_Buffer_Full_Get(Ring_Buffer_t pbuf)
 {
-    switch (Ring_Buffer_State_Get(pbuf)) {
-        case RINGBUFFER_EMPTY:
-            return 0;
-        case RINGBUFFER_FULL:
-            return pbuf->size;
-        case RINGBUFFER_HALFFULL:
-            if (pbuf->read_mirror == pbuf->write_mirror) {
-                return (pbuf->write_pos - pbuf->read_pos); // 如果镜像位相同，说明写指针在读指针之后
-            } else {
-                return (pbuf->size - (pbuf->read_pos - pbuf->write_pos)); // 如果镜像位不同，说明读指针在写指针之后
-            }
-        default:
-            return -1;
+    switch (Ring_Buffer_State_Get(pbuf))
+    {
+    case RINGBUFFER_EMPTY:
+        return 0;
+    case RINGBUFFER_FULL:
+        return pbuf->size;
+    case RINGBUFFER_HALFFULL:
+        if (pbuf->read_mirror == pbuf->write_mirror)
+        {
+            return (pbuf->write_pos - pbuf->read_pos); // 如果镜像位相同，说明写指针在读指针之后
+        }
+        else
+        {
+            return (pbuf->size - (pbuf->read_pos - pbuf->write_pos)); // 如果镜像位不同，说明读指针在写指针之后
+        }
+    default:
+        return -1;
     }
 }
 
@@ -54,7 +63,8 @@ int32_t Ring_Buffer_Read_OneByte(Ring_Buffer_t pbuf, uint8_t *ch)
 {
     assert(pbuf != NULL);
     // 首先判定是否为空
-    if (!Ring_Buffer_Full_Get(pbuf)) {
+    if (!Ring_Buffer_Full_Get(pbuf))
+    {
         return 0; // 空缓冲区，无法读取
     }
     *ch = pbuf->buf[pbuf->read_pos];
@@ -62,8 +72,10 @@ int32_t Ring_Buffer_Read_OneByte(Ring_Buffer_t pbuf, uint8_t *ch)
     if (pbuf->read_pos == pbuf->size - 1) // 读取完毕，读指针前进一位
     {
         pbuf->read_mirror = ~pbuf->read_mirror;
-        pbuf->read_pos    = 0;
-    } else {
+        pbuf->read_pos = 0;
+    }
+    else
+    {
         pbuf->read_pos++;
     }
     return 1;
@@ -76,7 +88,8 @@ int32_t Ring_Buffer_Write_OneByte(Ring_Buffer_t pbuf, const uint8_t ch)
 {
     assert(pbuf != NULL);
     // 首先判定是否为满
-    if (!Ring_Buffer_Empty_Get(pbuf)) {
+    if (!Ring_Buffer_Empty_Get(pbuf))
+    {
         return 0; // 满缓冲区，无法写入
     }
     pbuf->buf[pbuf->write_pos] = ch;
@@ -84,9 +97,33 @@ int32_t Ring_Buffer_Write_OneByte(Ring_Buffer_t pbuf, const uint8_t ch)
     if (pbuf->write_pos == pbuf->size - 1) // 读取完毕，写指针前进一位
     {
         pbuf->write_mirror = ~pbuf->write_mirror;
-        pbuf->write_pos    = 0;
-    } else {
+        pbuf->write_pos = 0;
+    }
+    else
+    {
         pbuf->write_pos++;
+    }
+    return 1;
+}
+
+/**
+ *  @brief  环形缓冲区写入多字节函数
+ */
+int32_t Ring_Buffer_Write(Ring_Buffer_t pbuf, const uint8_t *ch, int16_t length)
+{
+    assert(pbuf != NULL);
+    // 首先判定是否为满
+    if (!Ring_Buffer_Empty_Get(pbuf))
+    {
+        return 0; // 满缓冲区，无法写入
+    }
+    for (int i = 0; i < length; i++)
+    {
+        Ring_Buffer_Write_OneByte(pbuf, ch[i]);
+        if (!Ring_Buffer_Empty_Get(pbuf))
+        {
+            return 0;
+        }
     }
     return 1;
 }
@@ -98,10 +135,33 @@ int32_t Ring_Buffer_Peek_OneByte(Ring_Buffer_t pbuf, uint8_t *ch)
 {
     assert(pbuf != NULL);
     // 首先判定是否为空
-    if (Ring_Buffer_State_Get(pbuf) == RINGBUFFER_EMPTY) {
+    if (Ring_Buffer_State_Get(pbuf) == RINGBUFFER_EMPTY)
+    {
         return 0; // 空缓冲区，无法读取
     }
     *ch = pbuf->buf[pbuf->read_pos];
 
+    return 1;
+}
+
+/**
+ *  @brief  环形缓冲区读取多字节函数
+ */
+int32_t Ring_Buffer_Read(Ring_Buffer_t pbuf, uint8_t *ch, int16_t length)
+{
+    assert(pbuf != NULL);
+    // 首先判定是否为空
+    if (!Ring_Buffer_Full_Get(pbuf))
+    {
+        return 0; // 空缓冲区，无法读取
+    }
+    for (int i = 0; i < length; i++)
+    {
+        Ring_Buffer_Read_OneByte(pbuf, ch[i]);
+        if (!Ring_Buffer_Full_Get(pbuf))
+        {
+            return 0;
+        }
+    }
     return 1;
 }
